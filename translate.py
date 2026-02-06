64566291fc2f56fa2620ce1b44dfa5d0780904c4
Luke1 Lin <luke1.lin@intel.com>
2026-02-06 14:11:53 +0800

    Update translate.py
    First working draft

--- translate.py
+++ translate.py
@@ -1,4 +1,4 @@
-# This file is copied from https://github.com/openvinotoolkit/openvino.genai/blob/master/samples/python/visual_language_chat/visual_language_chat.py
+# This file is modified from https://github.com/openvinotoolkit/openvino.genai/blob/master/samples/python/visual_language_chat/visual_language_chat.py
 
 #!/usr/bin/env python3
 # Copyright (C) 2024 Intel Corporation
@@ -11,21 +11,8 @@ import openvino_genai
 from PIL import Image
 from openvino import Tensor
 from pathlib import Path
-
-
-def streamer(subword: str) -> bool:
-    '''
-
-    Args:
-        subword: sub-word of the generated text.
-
-    Returns: Return flag corresponds whether generation should be stopped.
-
-    '''
-    print(subword, end='', flush=True)
-
-    # No value is returned as in this example we don't want to stop the generation in this method.
-    # "return None" will be treated the same as "return openvino_genai.StreamingStatus.RUNNING".
+from transformers import AutoTokenizer
+import json
 
 
 def read_image(path: str) -> Tensor:
@@ -41,49 +28,102 @@ def read_image(path: str) -> Tensor:
     image_data = np.array(pic)
     return Tensor(image_data)
 
-
-def read_images(path: str) -> list[Tensor]:
-    entry = Path(path)
-    if entry.is_dir():
-        return [read_image(str(file)) for file in sorted(entry.iterdir())]
-    return [read_image(path)]
-
-
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('model_dir', help="Path to the model directory")
-    parser.add_argument('image_dir', help="Image file or dir with images")
-    parser.add_argument('device', nargs='?', default='CPU', help="Device to run the model on (default: CPU)")
+    parser.add_argument('--model_dir', required=True, help="Path to the model directory")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--text', help="Text file")
+    group.add_argument('--image', help="Image file")
+    parser.add_argument("--device", help="Device to run the model on (default: CPU)", choices=["CPU", "GPU", "NPU"], default="CPU")
+    parser.add_argument('--source_lang_code', required=True, help="Source language code, e.g., en or en-GB")
+    parser.add_argument('--target_lang_code', required=True, help="Target language code, e.g., zh or zh-TW")
     args = parser.parse_args()
 
-    rgbs = read_images(args.image_dir)
+    if args.text is not None:
+        content = "text"
+        with open(args.text, "r", encoding="utf-8") as f:
+            txt = f.read()
+    else:
+        content = "image"
+        rgb = read_image(args.image)
 
     # GPU and NPU can be used as well.
     # Note: If NPU is selected, only the language model will be run on the NPU.
     enable_compile_cache = dict()
-    if args.device == "GPU":
-        # Cache compiled models on disk for GPU to save time on the next run.
+    if args.device == "GPU" or args.device == "NPU":
+        # Cache compiled models on disk for GPU/NPU to save time on the next run.
         # It's not beneficial for CPU.
         enable_compile_cache["CACHE_DIR"] = "vlm_cache"
 
     pipe = openvino_genai.VLMPipeline(args.model_dir, args.device, **enable_compile_cache)
 
     config = openvino_genai.GenerationConfig()
-    config.max_new_tokens = 100
+    config.max_new_tokens = 2048
+    
+    if content == "text":
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "source_lang_code": args.source_lang_code,
+                        "target_lang_code": args.target_lang_code,
+                        "text": txt
+                    }
+                ],
+            }
+        ]
+    else:  # conten is "image"
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image",
+                        "source_lang_code": args.source_lang_code,
+                        "target_lang_code": args.target_lang_code,
+                        "image": "sample_image.png"   # file name doesn't care
+                    }
+                ],
+            }
+        ]
+    print(f"Messages:\n{messages}\n")
 
-    pipe.start_chat()
-    prompt = input('question:\n')
-    pipe.generate(prompt, images=rgbs, generation_config=config, streamer=streamer)
+    hf_tokenizer = AutoTokenizer.from_pretrained(args.model_dir, trust_remote_code=True)
 
-    while True:
-        try:
-            prompt = input("\n----------\n"
-                "question:\n")
-        except EOFError:
-            break
-        # New images and videos can be passed at each turn
-        pipe.generate(prompt, generation_config=config, streamer=streamer)
-    pipe.finish_chat()
+    prompt = hf_tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    print(f"Prompt:\n{prompt}\n")
+ 
+    # [WORKAROUND BEGINS]
+    # As OpenVINO GenAI pipeline hasn't yet supported TranslateGemma. OpenVINO GenAI pipeline somehow formats input text with Gemma3 chat_template (see formated messages below) but validates it using the TranslateGemma chat_template, which results in errors.
+    # formated messages=[{'content': '<bos><start_of_turn>user\nYou are a professional English (en) to
+    #                                 Japanese (ja) translator. Your goal is to accurately convey the
+    #                                 meaning and nuances of the original English text while adhering
+    #                                 to Japanese grammar, vocabulary, and cultural sensitivities.\n
+    #                                 Produce only the Japanese translation, without any additional
+    #                                 explanations or commentary. Please translate the following English
+    #                                 text into Japanese:\n\n\nhow are you doing today?<end_of_turn>\n<start_of_turn>model\n',
+    #                     'role': 'user'
+    #                    }]
+    # Here we workaround this by forcing OpenVINO GenAI pipeline to use Gemma3 chat_template for validation
+    GEMMA3_CHAT_TEMPLATE_FILE = "chat_template-gemma3.json"   # Downloaded from https://huggingface.co/google/gemma-3-4b-it/blob/main/chat_template.json
+    with open(GEMMA3_CHAT_TEMPLATE_FILE, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    GEMMA3_CHAT_TEMPLATE = data["chat_template"]
+    pipe.get_tokenizer().set_chat_template(GEMMA3_CHAT_TEMPLATE)
+    # [WORKAROUND ENDS]
+
+    if content == "text":
+        output = pipe.generate(prompt, generation_config=config)
+    else:  # conten is "image"
+        output = pipe.generate(prompt, images=rgb, generation_config=config)
+
+    print(f"Output:\n{output}\n")
 
 
 if '__main__' == __name__:
